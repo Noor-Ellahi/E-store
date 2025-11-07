305c1e5e90162c810dd714221e9dbf617881df45
Noor Elahi <noorellahi220@gmail.com>
2025-11-07 18:23:31 +0500

    Added User register system

--- src/app/(frontend)/register/page.js
+++ src/app/(frontend)/register/page.js
@@ -1,26 +1,46 @@
 "use client"
+import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useRef } from "react";
 import { FaArrowLeft } from "react-icons/fa";
+import { toast } from "react-toastify";
 // import { FaFacebook, FaGoogle } from "react-icons/fa";
 
 
 
 const Register = () => {
-    // const router = useRouter()
+    const router = useRouter()
+    const nameRef = useRef()
+    const emailRef = useRef()
+    const passRef = useRef()
+
+    const registerUser = async () => {
+        try {
+            const res = await axios.post(
+                '/api/auth/register',
+                { name: nameRef.current.value, email: emailRef.current.value, password: passRef.current.value, }
+            )
+            toast.success(res.data.message)
+            router.push('/')
+        } catch (error) {
+            toast.error(error.response.data.error)
+        }
+    }
+
+
 
 
     return (
         <div className="flex relative  justify-center items-center h-screen bg-[#FAFAFA]">
-            <Link href={"/"} className="absolute top-5 left-5 flex items-center gap-2 transition duration-500 hover:text-[#2240f5]"><FaArrowLeft className="text-sm"/>HOME</Link>
+            <Link href={"/"} className="absolute top-5 left-5 flex items-center gap-2 transition duration-500 hover:text-[#2240f5]"><FaArrowLeft className="text-sm" />HOME</Link>
             <div className="flex flex-col gap-5 pt-10 pb-15 px-5 rounded-xl items-start w-100 mx-10">
                 <div className="flex flex-col justify-start items-center w-[100%] pb-2  font-medium">
                     <h2 className="text-4xl font-serif pb-5 scale-y-150"> BAZAAR</h2>
                     <h1 className="text-xl pt-1 opacity-50 scale-y-120">Register</h1>
                 </div>
                 <div className="w-full relative">
-                    <input type="text" placeholder="Username" className="peer block bg-white py-2.5 pl-2 rounded-sm focus:border-blue-500 border-1 border-[#DADCE0] border-solid w-[100%] outline-none placeholder-transparent" />
+                    <input ref={nameRef} type="text" placeholder="Username" className="peer block bg-white py-2.5 pl-2 rounded-sm focus:border-blue-500 border-1 border-[#DADCE0] border-solid w-[100%] outline-none placeholder-transparent" />
                     <label
                         htmlFor="Name"
                         className="absolute left-3 bottom-full text-gray-500 text-sm transition-all
@@ -31,7 +51,7 @@ const Register = () => {
                     </label>
                 </div>
                 <div className="w-full relative">
-                    <input type="text" placeholder="Email" className="peer block bg-white py-2.5 pl-2 rounded-sm focus:border-blue-500 border-1 border-[#DADCE0] border-solid w-[100%] outline-none placeholder-transparent" />
+                    <input ref={emailRef} type="text" placeholder="Email" className="peer block bg-white py-2.5 pl-2 rounded-sm focus:border-blue-500 border-1 border-[#DADCE0] border-solid w-[100%] outline-none placeholder-transparent" />
                     <label
                         htmlFor="email"
                         className="absolute left-3 bottom-full text-gray-500 text-sm transition-all
@@ -42,7 +62,7 @@ const Register = () => {
                     </label>
                 </div>
                 <div className="w-full relative">
-                    <input type="password" placeholder="Password" className="peer block bg-white py-2.5 pl-2 rounded-sm focus:border-blue-500 border-1 border-[#DADCE0] border-solid w-[100%] outline-none placeholder-transparent" />
+                    <input ref={passRef} type="password" placeholder="Password" className="peer block bg-white py-2.5 pl-2 rounded-sm focus:border-blue-500 border-1 border-[#DADCE0] border-solid w-[100%] outline-none placeholder-transparent" />
                     <label
                         htmlFor="email"
                         className="absolute left-3 bottom-full text-gray-500 text-sm transition-all
@@ -54,7 +74,7 @@ const Register = () => {
                 </div>
                 {/* <h3 className="text-[#2240f5] text-sm cursor-pointer">Forget password?</h3> */}
 
-                <button className="w-full bg-[#2240f5] text-[#fff] py-2 rounded-sm transition hover:shadow-lg cursor-pointer">Register</button>
+                <button className="w-full bg-[#2240f5] text-[#fff] py-2 rounded-sm transition hover:shadow-lg cursor-pointer" onClick={registerUser}>Register</button>
 
                 <div className="flex justify-end w-[100%] pb-2 text-sm">
                     <p>Already have an account? <span className="font-medium text-[#2240f5] cursor-pointer"><Link href={"/login"}>Sign In</Link></span></p>

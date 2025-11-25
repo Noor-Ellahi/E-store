c068f8aeb56eeec7f1a418159c81881faac61e02
Noor Elahi <noorellahi220@gmail.com>
2025-11-26 01:49:35 +0500

    Last before deploy

--- src/app/api/admin/orders/route.js
+++ src/app/api/admin/orders/route.js
@@ -1,17 +1,17 @@
-import { verifyToken } from "@/app/lib/authJWT/auth";
-import Order from "@/app/modals/Order";
+// import { verifyToken } from "@/app/lib/authJWT/auth";
 // import Order from "@/app/modals/Order";
+// // import Order from "@/app/modals/Order";
 
 
 
-export async function GET(req) {
-  try {
-    const user = await verifyToken(req);
-    if (!user || user.role != "admin") return new Response(JSON.stringify({ error: "Forbidden" }), { status: 403 });
+// export async function GET(req) {
+//   try {
+//     const user = await verifyToken(req);
+//     if (!user || user.role != "admin") return new Response(JSON.stringify({ error: "Forbidden" }), { status: 403 });
 
-    const orders = await Order.find().populate("user").populate("products.productId");
-    return new Response(JSON.stringify(orders), { status: 200 });
-  } catch (err) {
-    return new Response(JSON.stringify({ error: err.message }), { status: 500 });
-  }
-}
+//     const orders = await Order.find().populate("user").populate("products.productId");
+//     return new Response(JSON.stringify(orders), { status: 200 });
+//   } catch (err) {
+//     return new Response(JSON.stringify({ error: err.message }), { status: 500 });
+//   }
+// }
